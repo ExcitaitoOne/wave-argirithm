878410da587a61b66dec9a0a903c0c414fed86ec
ExcitaitoOne <roja28@yandex.ru>
2022-10-07 20:22:26 +0700

    Сделал еще 2 задания

---
+++ HW1/exercise11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите первое число");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число");
+int B = int.Parse(Console.ReadLine() ?? "0");
+
+if (A % B == 0) {
+    Console.WriteLine("Число кратно");
+} else {
+    A = A % B;
+    Console.WriteLine("Остаток " +A);
+}
---
+++ HW1/exercise12/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int B = N;
+int i = 0;
+int count = 0;
+bool f = true;
+
+
+while (f) {
+N = N/10;
+count++;
+if (N<1) {
+    f = false;
+}
+} // нашел длину, count
+if (B<100) {
+    Console.WriteLine("нет третьего числа");
+}else{
+        B = B*1000;
+        while (i<count){
+        B = B/10;
+        i++;
+        }
+        B = B % 10;
+        Console.WriteLine("третье число = " +B);
+}
